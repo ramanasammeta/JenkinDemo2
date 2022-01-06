eed0bbebae6e6442200fe960c80b73c9ea207446
RamanaSametta <ramanasreddy@gmail.com>
2022-01-06 16:41:06 +0000

    updated

--- src/main/java/com/base/BaseClass.java
+++ src/main/java/com/base/BaseClass.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
     WebDriver driver;
