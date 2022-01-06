bedb0566191d2c9f4281a6ec496ca446ee21fc0c
RamanaSametta <ramanasreddy@gmail.com>
2022-01-06 10:59:13 +0000

    updated

--- src/main/java/com/base/BaseClass.java
+++ src/main/java/com/base/BaseClass.java
@@ -1,9 +1,11 @@
 package com.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.OperatingSystem;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
@@ -22,8 +24,8 @@ public class BaseClass {
              WebDriverManager.firefoxdriver().setup();
              driver=new FirefoxDriver();
          } else if (System.getProperty("browser").equalsIgnoreCase("IE")){
-             WebDriverManager.iedriver().setup();
-             driver=new InternetExplorerDriver();
+             WebDriverManager.edgedriver().operatingSystem(OperatingSystem.WIN).setup();
+             driver=new EdgeDriver();
          }
          driver.manage().window().maximize();
         driver.get(System.getProperty("url"));
